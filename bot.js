af15ad9c7ab0a6f5713aec273b1ba6d61157ecc0
Tomáš Huňa <jqueryforyou@gmail.com>
2017-04-22 21:48:45 +0200

    mongo connection test

--- bot.js
+++ bot.js
@@ -2,14 +2,26 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const request = require('request');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const port = process.env.PORT || 8080;
 const VERIFY_TOKEN = 'dont_worry_its_correct';
-const ACCESS_TOKEN = 'EAAF5ZA77yw88BAH98fnJtnxhNcg791ZBXPejS6ZCZCHmFaLeweeHfcE8JU1dp7q2W00CkkG5hqysHXoosFvzNMxP8zHP63i0MkAf2ROg1eN2ARDDgiKw1mdxE3TQyPB24jzzS95I7YB2jgZAxZCbJBGZA8PtZBDPSLzCCDATtaheFAZDZD';
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port);
+
+mongoose.connect('mongodb://' + process.env.DB_USER + ':' 
+                              + process.env.DB_PASS + '@ds115131.mlab.com:15131/game');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('connected');
+});
 
 app.get('/', function(req, res) {
     res.send('it works');
@@ -18,10 +30,8 @@ app.get('/', function(req, res) {
 app.get('/webhook', function(req, res) {
     if (req.query['hub.mode'] === 'subscribe' &&
         req.query['hub.verify_token'] === VERIFY_TOKEN) {
-        console.log('Validating webhook');
         res.status(200).send(req.query['hub.challenge']);
     } else {
-        console.error('Failed validation. Make sure the validation tokens match.');
         res.sendStatus(403);          
     }  
 });
@@ -29,67 +39,36 @@ app.get('/webhook', function(req, res) {
 app.post('/webhook', function (req, res) {
     var data = req.body;
 
-    // Make sure this is a page subscription
     if (data.object === 'page') {
-
-        // Iterate over each entry - there may be multiple if batched
         data.entry.forEach(function(entry) {
             var pageID = entry.id;
             var timeOfEvent = entry.time;
 
-            // Iterate over each messaging event
             entry.messaging.forEach(function(event) {
                 if (event.message) {
                     receivedMessage(event);
-                } else if (event.postback) {
-                    receivedPostback(event);
                 } else {
                     console.log("Webhook received unknown event: ", event);
                 }
             });
         });
 
-        // Assume all went well.
-        //
-        // You must send back a 200, within 20 seconds, to let us know
-        // you've successfully received the callback. Otherwise, the request
-        // will time out and we will keep trying to resend.
         res.sendStatus(200);
     }
 });
   
 function receivedMessage(event) {
-    // Putting a stub for now, we'll expand it in the following steps
-    console.log("Message data: ", event.message);
-
     var senderID = event.sender.id;
     var recipientID = event.recipient.id;
     var timeOfMessage = event.timestamp;
     var message = event.message;
 
-    console.log("Received message for user %d and page %d at %d with message:", 
-        senderID, recipientID, timeOfMessage);
-    console.log(JSON.stringify(message));
-
     var messageId = message.mid;
-
     var messageText = message.text;
     var messageAttachments = message.attachments;
 
     if (messageText) {
-
-        // If we receive a text message, check to see if it matches a keyword
-        // and send back the example. Otherwise, just echo the text we received.
-        switch (messageText) {
-            case 'generic':
-                sendGenericMessage(senderID);
-            break;
-
-            default:
-                sendTextMessage(senderID, messageText);
-        }
-    } else if (messageAttachments) {
-        sendTextMessage(senderID, "Message with attachment received");
+        sendTextMessage(senderID, messageText);
     }
 }
 
@@ -172,21 +151,4 @@ function callSendAPI(messageData) {
             console.error(error);
         }
     });  
-}
-
-function receivedPostback(event) {
-    var senderID = event.sender.id;
-    var recipientID = event.recipient.id;
-    var timeOfPostback = event.timestamp;
-
-    // The 'payload' param is a developer-defined field which is set in a postback 
-    // button for Structured Messages. 
-    var payload = event.postback.payload;
-
-    console.log("Received postback for user %d and page %d with payload '%s' " + 
-        "at %d", senderID, recipientID, payload, timeOfPostback);
-
-    // When a postback is called, we'll send a message back to the sender to 
-    // let them know it was successful
-    sendTextMessage(senderID, "Postback called");
 }
