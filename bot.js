141790ea5e4e93aa5e3d77b652794f21967843d0
Tomáš Huňa <jqueryforyou@gmail.com>
2017-04-23 09:13:11 +0200

    attachment test

--- bot.js
+++ bot.js
@@ -103,7 +103,7 @@ function checkUserMessage(user, messageText) {
         sendGenericMessage(user.id);
         user.generateNumber();
         user.save();
-        sendTextMessage(user.id, 'Well played! You have successfully guessed the right number.');
+        sendTextMessage(user.id, 'Well played! You have successfully guessed the right number. New number has been generated.');
     } else {
         sendTextMessage(user.id, 'Hmm.');
     }
@@ -116,38 +116,9 @@ function sendGenericMessage(recipientId) {
         },
         message: {
             attachment: {
-                type: "template",
+                type: 'image',
                 payload: {
-                    template_type: "generic",
-                    elements: [{
-                        title: "rift",
-                        subtitle: "Next-generation virtual reality",
-                        item_url: "https://www.oculus.com/en-us/rift/",               
-                        image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-                        buttons: [{
-                            type: "web_url",
-                            url: "https://www.oculus.com/en-us/rift/",
-                            title: "Open Web URL"
-                        }, {
-                            type: "postback",
-                            title: "Call Postback",
-                            payload: "Payload for first bubble",
-                        }],
-                    }, {
-                        title: "touch",
-                        subtitle: "Your Hands, Now in VR",
-                        item_url: "https://www.oculus.com/en-us/touch/",               
-                        image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-                        buttons: [{
-                            type: "web_url",
-                            url: "https://www.oculus.com/en-us/touch/",
-                            title: "Open Web URL"
-                        }, {
-                            type: "postback",
-                            title: "Call Postback",
-                            payload: "Payload for second bubble",
-                        }]
-                    }]
+                    'url': 'https://pbs.twimg.com/profile_images/665915626850902016/Zp5lUnYl.jpg'
                 }
             }
         }
